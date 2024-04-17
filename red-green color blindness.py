91300d6b9d982e9aff32a50a200a40cc8a1af4c3
joouiho <jjjj060222@gmail.com>
2024-04-17 10:50:27 +0900

    적록색약 최적화 실패
    주변 탐색 함수로 넘어가지 전에 i와 j가 i+-1, j+-1을 해도 0과 T를 넘어가지 않는지 확인하고 이를 검사하지 않아도 되는 다른 탐색함수로 보내려 함, 이러면 if문의 조건식이 3 => 2개로 줄어 더 좋을 것이라 예상 하지만 아님

--- red-green color blindness.py
+++ red-green color blindness.py
@@ -1,24 +1,40 @@
 import sys
 from collections import deque
 
+def trans(index, str): 
+    return str[:index] + 'n' + str[index+1:]
+
 def detectSurround(map, location, T):      #주변 같은 색상 있는지 판별
-    map[location[0]][location[1]] = 0   #map에 바로 지나간 자리 표시
-    if map[location[0]+1][location[1]] == map[location[0]][location[1]] and 0 <= location[0]+1 < T and map[location[0]+1][location[1]] == 0 :   #확인하려는 색과 같은지 확인
-        stack.append([location[0]+1, location[1]])
-        map[location[0]+1][location[1]] = 0   #위
+    if location[0]+1 < T  and map[location[0]+1][location[1]] == map[location[0]][location[1]] and map[location[0]+1][location[1]] != 'n':   #확인하려는 색과 같은지 확인
+        stack.append([location[0]+1, location[1]])   #위
         
-    if  map[location[0]-1][location[1]] == map[location[0]][location[1]] and 0 <= location[0]-1 < T and map[location[0]-1][location[1]] == 0 :
-        stack.append([location[0]-1, location[1]])
-        map[location[0]-1][location[1]] = 0   #아래
+    if 0 <= location[0]-1 and map[location[0]-1][location[1]] == map[location[0]][location[1]] and map[location[0]-1][location[1]] != 'n' :
+        stack.append([location[0]-1, location[1]])#아래
+        
+    if location[1]+1 < T  and map[location[0]][location[1]+1] == map[location[0]][location[1]] and map[location[0]][location[1]+1] != 'n':
+        stack.append([location[0], location[1]+1])#우
 
-    if map[location[0]][location[1]+1] == map[location[0]][location[1]] and 0 <= location[1]+1 < T and map[location[0]][location[1]+1] == 0  :
-        stack.append([location[0], location[1]+1])
-        map[location[0]][location[1]+1] = 0   #우
-        
-    if map[location[0]][location[1]-1] == map[location[0]][location[1]] and 0 <= location[1]-1 < T and map[location[0]][location[1]-1] == 0  :
+    if 0 <= location[1]-1 and map[location[0]][location[1]-1] == map[location[0]][location[1]] and map[location[0]][location[1]-1] != 'n':
         stack.append([location[0], location[1]-1])  #좌
-        map[location[0]][location[1]-1] = 0
+    
+    map[location[0]] = trans(location[1], map[location[0]])   #map에 바로 지나간 자리 표시
 
+def detectSurround1(map, location, T):      #주변 같은 색상 있는지 판별
+    if map[location[0]+1][location[1]] == map[location[0]][location[1]] and map[location[0]+1][location[1]] != 'n':   #확인하려는 색과 같은지 확인
+        stack.append([location[0]+1, location[1]])   #위
+        
+    if map[location[0]-1][location[1]] == map[location[0]][location[1]] and map[location[0]-1][location[1]] != 'n' :
+        stack.append([location[0]-1, location[1]])#아래
+        
+    if  map[location[0]][location[1]+1] == map[location[0]][location[1]] and map[location[0]][location[1]+1] != 'n':
+        stack.append([location[0], location[1]+1])#우
+
+    if map[location[0]][location[1]-1] == map[location[0]][location[1]] and map[location[0]][location[1]-1] != 'n':
+        stack.append([location[0], location[1]-1])  #좌
+    
+    map[location[0]] = trans(location[1], map[location[0]])   #map에 바로 지나간 자리 표시
+
+sys.stdin = open("D:/beakjoon/beakjoon/redgreen.txt",'r')
 T = int(sys.stdin.readline())
 map = []
 blindMap = []
@@ -33,19 +49,26 @@ for i in range(T):
 
 for i in range(T):
     for j in range(T):
-        if map[i][j] == 0:
+        if map[i][j] != 'n':
             detectSurround(map, [i,j], T)
             while stack:
-                detectSurround(map, stack.pop(), T)
+                if stack:
+                    t = stack[-1]
+                    if 0<t[0]<T-1 and 0<t[1]<T-1 :
+                        detectSurround1(map, stack.pop(), T)
+                    else:
+                        detectSurround(map, stack.pop(), T)
             sectorNum += 1
-map = [[0 for i in range(T)] for i in range(T)]
 
-for i in range(T):
-    for j in range(T):
-        if map[i][j] == 0:
-            detectSurround(blindMap, map, [i,j], T)
+        if blindMap[i][j] != 'n':
+            detectSurround(blindMap, [i,j], T)
             while stack:
-                detectSurround(blindMap, map, stack.pop(), T)
+                if stack:
+                    t = stack[-1]
+                    if 0<t[0]<T-1 and 0<t[1]<T-1 :
+                        detectSurround1(blindMap, stack.pop(), T)
+                    else:
+                        detectSurround(blindMap, stack.pop(), T)
             blindSectorNum += 1
 
 print (sectorNum, blindSectorNum)
