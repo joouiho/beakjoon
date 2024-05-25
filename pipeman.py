4ad45e31e67ec502c220a16bfa60a129cd225f49
joouiho <jjjj060222@gmail.com>
2024-05-25 14:50:30 +0900

    피리부는 남자 변수명 수정

--- pipeman.py
+++ pipeman.py
@@ -1,7 +1,7 @@
 #https://www.acmicpc.net/problem/16724
 from collections import deque
 import sys
-def dfs( lc, map, visited, pathNum):
+def search( lc, map, visited, pathNum):
     dict = {'U':-1, "D":1, "L":-1, "R":1}
     state = map[lc[0]][lc[1]]
     visited[lc[0]][lc[1]] = pathNum
@@ -15,19 +15,22 @@ def dfs( lc, map, visited, pathNum):
         return 1
     elif visited[move[0]][move[1]] != pathNum:
         return 0
-sys.stdin = open (r"D:\beakjoon\beakjoon\text\pipeman.txt",'r')
-info = list(map(int, sys.stdin.readline().split()))
+
+# sys.stdin = open (r"D:\beakjoon\beakjoon\text\pipeman.txt",'r')
+
+# info = list(map(int, sys.stdin.readline().split()))
+info = list(map(int,input().split()))
 map = [[x for x in sys.stdin.readline().strip()] for _ in range(info[0])]
 visited = [[0]*info[1] for _ in range(info[0])]
 stack = deque()
-sum = 0
+loopNum = 0
 pathNum = 1
 for i in range(info[0]):
     for j in range(info[1]):
         if visited[i][j] == 0:
             stack.append([i,j])
             while (stack):
-                loop = dfs(stack.pop(), map, visited, pathNum)
-            sum+=loop
+                loop = search(stack.pop(), map, visited, pathNum)
+            loopNum+=loop
             pathNum+=1
-print (sum)
+print (loopNum)
