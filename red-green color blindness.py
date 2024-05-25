8a64a9195fb16141a4655793d9f4a33c6ae20c2e
joouiho <jjjj060222@gmail.com>
2024-05-25 14:58:18 +0900

    적록색약 원상복구

--- red-green color blindness.py
+++ red-green color blindness.py
@@ -2,36 +2,25 @@ import sys
 import copy
 from collections import deque
 
-def detectSurround(map, visited, lc, T):      #주변 같은 색상 있는지 판별
-    visited[lc[0]][lc[1]] = 1   #시작자리
-    try:  
-        if map[lc[0]+1][lc[1]] == map[lc[0]][lc[1]] and visited[lc[0]+1][lc[1]] == 0:    #확인하려는 색과 같은지 확인
-            stack.append([lc[0]+1, lc[1]])
-            visited[lc[0]+1][lc[1]] = 1   #위
-    except:
-        a = 1
-    
-    try:  
-        if  map[lc[0]-1][lc[1]] == map[lc[0]][lc[1]] and visited[lc[0]-1][lc[1]] == 0 :
-            stack.append([lc[0]-1, lc[1]])
-            visited[lc[0]-1][lc[1]] = 1   #아래
-    except:
-        a =1
+def detectSurround(map, visited, location, T):      #주변 같은 색상 있는지 판별
+    visited[location[0]][location[1]] = 1   #시작자리
+    if  0 <= location[0]+1 < T and visited[location[0]+1][location[1]] == 0 and map[location[0]+1][location[1]] == map[location[0]][location[1]] :   #확인하려는 색과 같은지 확인
+        stack.append([location[0]+1, location[1]])
+        visited[location[0]+1][location[1]] = 1   #위
+        
+    if  0 <= location[0]-1 < T and visited[location[0]-1][location[1]] == 0 and map[location[0]-1][location[1]] == map[location[0]][location[1]] :
+        stack.append([location[0]-1, location[1]])
+        visited[location[0]-1][location[1]] = 1   #아래
 
-    try  :
-        if map[lc[0]][lc[1]+1] == map[lc[0]][lc[1]] and visited[lc[0]][lc[1]+1] == 0 :
-            stack.append([lc[0], lc[1]+1])
-            visited[lc[0]][lc[1]+1]   #우
-    except:
-        a =1
+    if  0 <= location[1]+1 < T and visited[location[0]][location[1]+1] == 0 and map[location[0]][location[1]+1] == map[location[0]][location[1]] :
+        stack.append([location[0], location[1]+1])
+        visited[location[0]][location[1]+1]   #우
+        
+    if 0 <= location[1]-1 < T and visited[location[0]][location[1]-1] == 0 and map[location[0]][location[1]-1] == map[location[0]][location[1]] :
+        stack.append([location[0], location[1]-1])  #좌
+        visited[location[0]][location[1]-1] = 1
 
-    try :
-        if map[lc[0]][lc[1]-1] == map[lc[0]][lc[1]] and visited[lc[0]][lc[1]-1] == 0 :
-            stack.append([lc[0], lc[1]-1])  #좌
-            visited[lc[0]][lc[1]-1] = 1
-    except:
-        a =1
-sys.stdin = open('D:/beakjoon/beakjoon/text/redgreen.txt', 'r')
+sys.stdin = open (r"D:\beakjoon\beakjoon\text\redgreen.txt", 'r')
 T = int(sys.stdin.readline())
 map = []
 sectorNum = 0
@@ -39,28 +28,27 @@ blindSectorNum = 0
 stack = deque()
 blindMap = copy.deepcopy(map)
 visited = [[0 for i in range(T)] for i in range(T)]
-cvisited = copy.deepcopy(visited)
+
 for i in range(T):
     temp = sys.stdin.readline().replace("\n","")
     map.append(temp)
     blindMap.append(temp.replace("R", "G"))
 
-
 for i in range(T):
     for j in range(T):
-        print (map[i-1][j])
         if visited[i][j] == 0:
             detectSurround(map, visited, [i,j], T)
             while stack:
                 detectSurround(map, visited, stack.pop(), T)
             sectorNum += 1
+visited = [[0 for i in range(T)] for i in range(T)]
 
 for i in range(T):
     for j in range(T):
-        if cvisited[i][j] == 0:
-            detectSurround(blindMap, cvisited, [i,j], T)
+        if visited[i][j] == 0:
+            detectSurround(blindMap, visited, [i,j], T)
             while stack:
-                detectSurround(blindMap, cvisited, stack.pop(), T)
+                detectSurround(blindMap, visited, stack.pop(), T)
             blindSectorNum += 1
 
 print (sectorNum, blindSectorNum)
